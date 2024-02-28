68183164697a3748e35bae7c6e1092860ae8ee85
faroukm2002 <faroukm238@gamil.com>
2024-02-28 03:12:08 +0200

    error  handling

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,13 +1,17 @@
+import { globalError } from "./middleware/globalErrorMiddleware.js";
 import teacherRouter from "./modules/teacher/teacher.routes.js"
+import { AppError } from "./utils/AppError.js";
 
 export function bootstrap(app){
     app.get('/', (req, res) => res.send('Hello World!'))  
 
     app.use("/api/v1/teacher",teacherRouter)
 
+  // url error
+  app.use("*", (req, res, next) => {
+    next(new AppError(`invalid url ${req.originalUrl}`, 404));
+  });
 
-
+  // globalError
+  app.use(globalError);
 }
-
-
-
--- src/modules/teacher/teacher.controller.js
+++ src/modules/teacher/teacher.controller.js
@@ -1,55 +1,57 @@
 import { teacherModel } from "../../../database/models/teacher.models.js"
+import { AppError } from "../../utils/AppError.js"
+import { catchError } from "../../utils/catchError.js"
 
-const addTeacher = async(req,res,next) => {
+const addTeacher = catchError(async(req,res,next) => {
     // let teachers=await teacherModel.findOne({email:req.body.email})
     // if(teachers) return next(new AppError("duplicate email",409))
     const teacher =new teacherModel(req.body)
     await teacher.save()
     res.status(201).json({message:"Done",teacher})
 
-}
-const getAllteachers= async(req,res,next)=>{
+})
+const getAllteachers=catchError(async(req,res,next)=>{
     const teachers= await teacherModel.find()
     // created
     res.status(201).json({message:"Done",teachers})
     
   
 
-    }
+    })
   
-    const getTeacherByID=async(req,res,next)=>{
+    const getTeacherByID=catchError(async(req,res,next)=>{
 
         const teacher=await teacherModel.findById(req.params.id)
         res.status(201).json({message:"Done",teacher})  
-    }
+    })
      
 
-const updateTeacher= async(req,res,next)=>{
+const updateTeacher= catchError(async(req,res,next)=>{
     const{id}=req.params
     const teacher=await teacherModel.findByIdAndUpdate(
         id,
         req.body,
         {new:true}
     )
-      !teacher && res.status(404).json({message:"teacher not found",}) 
+    !teacher && next(new AppError('teacher not found',404))
 
       teacher &&   res.status(201).json({message:"Done",teacher})
 }
+)
 
 
-
- const deleteTeacher=  async(req,res,next)=>{
+ const deleteTeacher=  catchError(async(req,res,next)=>{
     const{id}=req.params
     const teacher= await teacherModel.findByIdAndDelete(id) //{}
 
 
       //!{}==false
-    !teacher && next(new AppError(`${name} not found`,404))
+    !teacher && next(new AppError('teacher not found',404))
    
     teacher &&   res.status(201).json({message:"Done",teacher})
  
 
-  }
+  })
 
 
 
